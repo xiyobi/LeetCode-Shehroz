90f128b71343229aeeb2ac942f0a836fc05577b4
xiyobi <xiyobidasturchi0011@gmail.com>
2024-09-05 12:38:38 +0500

    Update 26.py
    hatosi bor edi togrilndi

--- 26.py
+++ 26.py
@@ -1,11 +1,10 @@
 def removeDuplicates( nums):
-        dc ={}
-        for i in nums:
-            dc[i] = nums.count(i)
-        n = list((dc.keys()))
-        nums = n.copy()
+        son = set(nums)
+        nums.clear()
+        for i in son:
+                nums.append(i)
+        nums.sort()
         return len(nums)
-
 
 son = [0,0,1,1,1,2,2,3,3,4]
 natija =removeDuplicates(son)            
