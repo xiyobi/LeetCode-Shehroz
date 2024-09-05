a858d47e1a0b041bc753548f7cf4e15d67229461
xiyobi <xiyobidasturchi0011@gmail.com>
2024-09-05 12:24:21 +0500

    Update 190.py
    Toliq ishlandi salkamchiligi bor 
    05.09.2004

--- 190.py
+++ 190.py
@@ -1,8 +1,8 @@
 
 def ila(a: int):
-        # a = a[::-1]
-        print(a,type(a))
-        x=int(a,2)
+        s = bin(a)[2:][::-1]
+        s = s + "0"*(32 - len(s))
+        x=int(s,2)
         return x
 lst = "00111001011110000010100101000000"
 natija = ila(lst)
